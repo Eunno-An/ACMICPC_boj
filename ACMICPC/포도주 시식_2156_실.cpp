4e423e75167afe039adb0d99f552f58abf0b9e14
Eunno An <12171647@inha.edu>
2021-01-30 22:47:14 +0900

    1/30

--- ACMICPC/포도주 시식_2156_실.cpp
+++ ACMICPC/포도주 시식_2156_실.cpp
@@ -1,59 +1,35 @@
+#include <stdio.h>
 
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <climits>
-#include <cstring>
-#include <string>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <map>
-#include<iomanip>
-
-using namespace std;
-
-/*
-- endl 대신 '\n' 사용하기
-- cin.tie(0) 사용
-- 테스트 케이스 있는 문제일 시 전역변수 초기화 신경쓰기
-- (A + B + C) % D = ((A + B) % D + C) % D
-- 문자열 출력 문제는 정답 문자열 복사해서 코드에 넣기
-- 괄호 사용 유의하기
-- 문자열은 함수로 넘길 때 const & 잘 사용하기
-
-- 최악의 경우 int 값 초과하는지, 배열 인덱스 초과하는지 확인
-- n 범위 확인 (0인 경우), 양수음수 정수소수 확인, 불가케이스 -1 출력 등 확인
-- 큰 배열 선언 시 전역선언, 테케 많을 시 초기화, 배열 용량 max N + 5
-*/
-
-#define MOD 1000000007
-#define INT_MAX 987654321
-#define MAX 100005
-
-typedef long long int ll;
-typedef pair<int, int> pii;
-
-
-// 테스트 케이스 초기화 시
-void init()
-{
-
+int max(int a, int b) {
+    return a > b ? a : b;
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+int main(void) {
 
-	init();
+    int N;
+    int wine[10010] = {};
+    int Dp[10010] = {};
+    scanf("%d", &N);
 
+    for (int i = 1; i <= N; i++)
+        scanf("%d", &wine[i]);
 
+    for (int i = 1; i < 3 && i <= N; i++) {
+        if (i == 1)
+            Dp[i] = wine[i];
+        else
+            Dp[i] = wine[i] + wine[i - 1];
+    }
 
-	return 0;
+    for (int i = 3; i <= N; i++) {
+        int result = 0;
+        result = max(wine[i] + Dp[i - 2], Dp[i - 1]);
+        result = max(result, wine[i] + wine[i - 1] + Dp[i - 3]);
+        Dp[i] = result;
+    }
+    //와인을 마시는 경우
+    //dp[i-1] : 현재 꺼 안먹을 때
+    //w[i] + dp[i-2]: 현재꺼 먹고 이전꺼 안먹을 때
+    //w[i] + w[i-1] + dp[i-3] : 현재꺼 먹고 이전꺼 먹고
+    printf("%d\n", Dp[N]);
 }
