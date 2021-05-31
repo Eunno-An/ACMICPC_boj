8e5ec69b3379189834621a65bf415337c7fba53a
Eunno An <12171647@inha.edu>
2021-05-31 20:14:01 +0900

    Update and rename 행운의 문자열.cpp to 1342_행운의 문자열.cpp

--- 백트랙킹/1342_행운의 문자열.cpp
+++ 백트랙킹/1342_행운의 문자열.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 string S;
 //순열 구하기
-//
 int main() {
 	cin >> S; //알파벳 소문자로만 이루어져 있다.
 	//인접해 있는 모든 문자가 같지 않은 문자열 = 행운의 문자열
@@ -14,7 +13,7 @@ int main() {
 	sort(S.begin(), S.end());
 
 	do {
-		cout << S << '\n';
+		//cout << S << '\n';
 		bool flag = false;
 		for (int i = 1; i < S.size(); i++) {
 			if (S[i - 1] == S[i]) {
@@ -27,5 +26,6 @@ int main() {
 		}
 	}while(next_permutation(S.begin(), S.end()));
 
+	cout << cnt << '\n';
 	return 0;
 }
